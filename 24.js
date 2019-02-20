b2bc1f02af29d789ca25aa6159c0d49e759d9afd
i2ui3 <47023306+i2ui3@users.noreply.github.com>
2019-02-20 22:34:33 +0800

    Update 24.js

--- 24.js
+++ 24.js
@@ -1,1 +1,28 @@
+// 自动推送百度
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
 
+// 统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2612372228af264db0d2bba2158e58a8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+// 自动推送360
+(function(){
+var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?c937dff1e890f9c58e62e303ce2021e2":"https://jspassport.ssl.qhimg.com/11.0.1.js?c937dff1e890f9c58e62e303ce2021e2";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
